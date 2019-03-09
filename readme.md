81fe4e9dbd0930072a8903253500858f9d7b6195
susanjiang03 <susanjiang03@gmail.com>
2019-03-08 23:22:06 -0500

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+### Sign Up Application for UCBA
---
+++ src/main/java/com/ucba/www/lineup/LineupApplication.java
@@ -0,0 +1,13 @@
+package com.ucba.www.lineup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineupApplication.class, args);
+	}
+
+}
